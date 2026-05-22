e7f9fff6d735bc330c7c27e0496679c78171d763
ngoye <133608048+nuhgoye@users.noreply.github.com>
2026-05-22 19:33:39 +0000

    sections

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 import heart from './assets/heart.png'
 import pfp from './assets/pfp.png'
 
 function App() {
+  const [page, setPage] = useState('home')
+
+  if (page === 'page2') {
+    return (
+      <main className="page2">
+        <button className="back" onClick={() => setPage('home')}>
+          back
+        </button>
+        <div className="page2-inner">
+          <button className="page2-button">about me</button>
+          <button className="page2-button">skills n stuff</button>
+          <button className="page2-button">contacts</button>
+        </div>
+      </main>
+    )
+  }
+
   return (
     <main className="landing">
       <div className="landing-inner">
@@ -10,7 +28,9 @@ function App() {
           <h1 className="title">ngoye diop.</h1>
           <img src={heart} alt="heart" className="heart" />
           <p className="subtitle">my life condensed into a website!</p>
-          <button className="enter">enter my world</button>
+          <button className="enter" onClick={() => setPage('page2')}>
+            enter my world
+          </button>
         </div>
 
         <div className="right">
